f6d00f98237e2f2188265d1b11114ecfd6fe3e24
denis.zhdanov <denzhdanov@gmail.com>
2022-08-22 12:15:10 +0800

    added <uuid> meta-value support

--- harmonysoft-common-test/src/main/kotlin/tech/harmonysoft/oss/test/fixture/meta/value/CommonFixtureMetaValueMapper.kt
+++ harmonysoft-common-test/src/main/kotlin/tech/harmonysoft/oss/test/fixture/meta/value/CommonFixtureMetaValueMapper.kt
@@ -5,6 +5,7 @@ import tech.harmonysoft.oss.common.ProcessingResult.Companion.failure
 import tech.harmonysoft.oss.common.ProcessingResult.Companion.success
 import tech.harmonysoft.oss.common.time.clock.ClockProvider
 import tech.harmonysoft.oss.test.fixture.CommonTestFixture
+import java.util.*
 import javax.inject.Named
 
 @Named
@@ -17,6 +18,7 @@ class CommonFixtureMetaValueMapper(
     override fun map(context: Any, metaValue: String): ProcessingResult<String?, Unit> {
         return when (metaValue) {
             "time-zone" -> success(clockProvider.data.zone.id)
+            "uuid" -> success(UUID.randomUUID().toString())
             else -> failure(Unit)
         }
     }
