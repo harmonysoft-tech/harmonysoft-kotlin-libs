3ef35506b787d49ac9f0771ac8b823ed33f4faa8
denis.zhdanov <denzhdanov@gmail.com>
2022-08-23 15:55:43 +0800

    introduced a constant for library step definitions path

---
+++ harmonysoft-common-cucumber/src/main/kotlin/tech/harmonysoft/oss/cucumber/CucumberConstants.kt
@@ -0,0 +1,9 @@
+package tech.harmonysoft.oss.cucumber
+
+object CucumberConstants {
+
+    /**
+     * Defines a package which contains all library step definitions
+     */
+    const val GLUE_PATH = "tech.harmonysoft.oss.cucumber.glue"
+}
