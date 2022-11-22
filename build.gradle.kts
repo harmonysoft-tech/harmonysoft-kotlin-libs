4d6fc2f6131c366e4509b32c55567900ecc529b1
Denis Zhdanov <denis.zhdanov@dkatalis.com>
2022-11-22 11:21:10 +0800

    configured release paperwork plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+    id("tech.harmonysoft.oss.gradle.release.paperwork") version "1.4.0"
 }
 
 group = "tech.harmonysoft"
 version = Version.APP
+
+releasePaperwork {
+    projectVersionFile.set("buildSrc/src/main/kotlin/Version.kt")
+    projectVersionRegex.set("""const val APP = "([^"]+)"""")
+}
 
 nexusPublishing {
     repositories {
