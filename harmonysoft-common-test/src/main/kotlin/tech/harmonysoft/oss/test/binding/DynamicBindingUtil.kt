913817f743081a3daacfff1977c73716cba6d7f3
Denis Zhdanov <denis.zhdanov@dkatalis.com>
2023-05-12 12:43:03 +0800

    correct binding regex

--- harmonysoft-common-test/src/main/kotlin/tech/harmonysoft/oss/test/binding/DynamicBindingUtil.kt
+++ harmonysoft-common-test/src/main/kotlin/tech/harmonysoft/oss/test/binding/DynamicBindingUtil.kt
@@ -5,7 +5,7 @@ object DynamicBindingUtil {
     /**
      * Common regex for capturing dynamic value to bind.
      */
-    val TO_BIND_REGEX = """<bind:(.+)>""".toRegex()
+    val TO_BIND_REGEX = """<bind:([^>]+)>""".toRegex()
 
     val DYNAMIC_BOUND_VALUE_REGEX = """bound:(.+)""".toRegex()
 }
