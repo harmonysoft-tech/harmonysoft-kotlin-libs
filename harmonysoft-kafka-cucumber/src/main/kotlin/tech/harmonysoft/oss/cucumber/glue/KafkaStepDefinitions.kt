7c72cced2ab81c9aafccf2d73ff681dfcbc96a4f
Denis Zhdanov <denis.zhdanov@dkatalis.com>
2023-05-26 17:30:18 +0800

    move common functionality form kafka-cucumber to kafka-test

--- harmonysoft-kafka-cucumber/src/main/kotlin/tech/harmonysoft/oss/cucumber/glue/KafkaStepDefinitions.kt
+++ harmonysoft-kafka-cucumber/src/main/kotlin/tech/harmonysoft/oss/cucumber/glue/KafkaStepDefinitions.kt
@@ -1,47 +1,15 @@
 package tech.harmonysoft.oss.cucumber.glue
 
 import io.cucumber.java.en.Given
-import org.apache.kafka.clients.producer.KafkaProducer
-import org.apache.kafka.clients.producer.ProducerConfig
-import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.serialization.StringSerializer
-import tech.harmonysoft.oss.kafka.config.TestKafkaConfigProvider
-import tech.harmonysoft.oss.kafka.fixture.KafkaFixtureContext
-import tech.harmonysoft.oss.kafka.fixture.KafkaTestFixture
-import tech.harmonysoft.oss.test.fixture.FixtureDataHelper
 import javax.inject.Inject
+import tech.harmonysoft.oss.kafka.service.TestKafkaManager
 
 class KafkaStepDefinitions {
 
-    @Inject private lateinit var configProvider: TestKafkaConfigProvider
-    @Inject private lateinit var fixtureHelper: FixtureDataHelper
-
-    private fun withProducer(action: (KafkaProducer<String, String>) -> Any) {
-        val config = configProvider.data
-        val producer = KafkaProducer<String, String>(
-            mapOf(
-                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to "${config.host}:${config.port}",
-                ProducerConfig.CLIENT_ID_CONFIG to "test",
-                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
-                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java
-            )
-        )
-        try {
-            action(producer)
-        } finally {
-            producer.close()
-        }
-    }
+    @Inject private lateinit var kafka: TestKafkaManager
 
     @Given("^the following kafka message is sent to topic ([^\\s]+):$")
     fun sendMessage(topic: String, message: String) {
-        withProducer {
-            val withExpandedMetaData = fixtureHelper.prepareTestData(
-                type = KafkaTestFixture.TYPE,
-                context = KafkaFixtureContext(topic),
-                data = message
-            ).toString()
-            it.send(ProducerRecord(topic, withExpandedMetaData))
-        }
+        kafka.sendMessage(topic, message)
     }
 }
---
+++ harmonysoft-kafka-test/src/main/kotlin/tech/harmonysoft/oss/kafka/service/TestKafkaManager.kt
@@ -0,0 +1,46 @@
+package tech.harmonysoft.oss.kafka.service
+
+import javax.inject.Named
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringSerializer
+import tech.harmonysoft.oss.kafka.config.TestKafkaConfigProvider
+import tech.harmonysoft.oss.kafka.fixture.KafkaFixtureContext
+import tech.harmonysoft.oss.kafka.fixture.KafkaTestFixture
+import tech.harmonysoft.oss.test.fixture.FixtureDataHelper
+
+@Named
+class TestKafkaManager(
+    private val configProvider: TestKafkaConfigProvider,
+    private val fixtureHelper: FixtureDataHelper
+) {
+
+    fun withProducer(action: (KafkaProducer<String, String>) -> Any) {
+        val config = configProvider.data
+        val producer = KafkaProducer<String, String>(
+            mapOf(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to "${config.host}:${config.port}",
+                ProducerConfig.CLIENT_ID_CONFIG to "test",
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java
+            )
+        )
+        try {
+            action(producer)
+        } finally {
+            producer.close()
+        }
+    }
+
+    fun sendMessage(topic: String, message: String) {
+        withProducer {
+            val withExpandedMetaData = fixtureHelper.prepareTestData(
+                type = KafkaTestFixture.TYPE,
+                context = KafkaFixtureContext(topic),
+                data = message
+            ).toString()
+            it.send(ProducerRecord(topic, withExpandedMetaData))
+        }
+    }
+}
