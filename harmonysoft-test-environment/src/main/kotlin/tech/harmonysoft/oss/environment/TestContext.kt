383ea45662a591d060b34d51e1f66a95d14361e2
Denis Zhdanov <denis.zhdanov@dkatalis.com>
2023-06-16 13:19:58 +0700

    add utility TestContext.prepareDirectory() method

--- harmonysoft-test-environment/src/main/kotlin/tech/harmonysoft/oss/environment/TestContext.kt
+++ harmonysoft-test-environment/src/main/kotlin/tech/harmonysoft/oss/environment/TestContext.kt
@@ -1,8 +1,25 @@
 package tech.harmonysoft.oss.environment
 
 import java.io.File
+import tech.harmonysoft.oss.test.util.TestUtil
 
 data class TestContext(
     val rootDir: File,
     val executionId: String
-)
+) {
+
+    fun prepareDirectory(path: String): File {
+        val result = File(rootDir, path)
+        if (result.isDirectory) {
+            return result
+        }
+        if (result.exists()) {
+            TestUtil.fail("can't create test directory ${result.canonicalPath} - there is a file at the target path")
+        }
+        val ok = result.mkdirs()
+        if (!ok) {
+            TestUtil.fail("can't create auxiliary directory ${result.canonicalPath}")
+        }
+        return result
+    }
+}
